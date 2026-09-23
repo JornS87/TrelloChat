7dc515e956998d5f0e49042d83a995578879d215
JornS87 <149232277+JornS87@users.noreply.github.com>
2026-09-23 21:43:06 +0200

    Modify chat popup URL and dimensions
    Updated chat popup URL version and height.

--- client.js
+++ client.js
@@ -9,8 +9,8 @@ window.TrelloPowerUp.initialize({
 
         return t.popup({
           title: '💬 Badfellas Chat',
-          url: './chat.html?v=13',
-          height: 650
+          url: './chat.html?v=14',
+          height: 700
         });
 
       }
