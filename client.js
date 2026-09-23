6296156d81593396e2f2fbd3561248865a7d840a
JornS87 <149232277+JornS87@users.noreply.github.com>
2026-09-23 21:49:32 +0200

    Change chat button to Development Chat and use modal
    Updated chat button to reflect development context and changed popup to modal.

--- client.js
+++ client.js
@@ -3,13 +3,14 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options) {
 
     return [{
-      text: '💬 Chat',
+      text: '💬 Development Chat',
 
       callback: function(t) {
 
-        return t.popup({
-          title: '💬 Badfellas Chat',
-          url: './chat.html?v=14',
+        return t.modal({
+          url: './chat.html?v=15',
+          fullscreen: false,
+          title: '💬 Badfellas Development Chat',
           height: 700
         });
 
