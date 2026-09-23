be55a86fdc31858ec544a16f86ea870a9389eb96
JornS87 <149232277+JornS87@users.noreply.github.com>
2026-09-23 19:15:28 +0200

    Modify chat URL in Trello Power-Up initialization
    Updated chat URL to include versioning.

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ window.TrelloPowerUp.initialize({
       callback: function(t) {
 
         return t.boardBar({
-          url: './chat.html',
+          url: './chat.html?v=2',
           height: 500,
           resizable: true,
           title: '💬 Team Chat'
